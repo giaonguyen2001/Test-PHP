3868005dd4616f332f9867f7547e9d59249dac10
giaonguyen2001 <giaonguyen.be@gmail.com>
2021-08-12 10:18:15 +0700

    First Gitlab

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Form</title>
+   </head>
+   <body>
+      <form action="welcome.php" method="post">
+         Name:<br> <input type="text" name="name"><br>
+         Email:<br> <input type="text" name="email"><br>
+         <input type="submit">
+      </form>
+      <hr>
+      <!-- Welcome: <?php echo $_POST["name"]; ?> <br>
+      Your email address is: <?php echo $_POST["email"]; ?> <br> -->
+      
+   </body>
+</html>
